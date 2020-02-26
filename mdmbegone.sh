66352f7ffc1d34fd222cc0985e3db03c62b9bcf6
Benjamin G <therealrandomblock1@gmail.com>
2020-02-26 16:27:13 -0600

    initial commit

---
+++ mdmbegone.sh
@@ -0,0 +1,3 @@
+echo MDMBegone by Randomblock1
+rm -rf /private/var/containers/Shared/SystemGroup/systemgroup.com.apple.configurationprofiles/
+echo $'\nAll Done!\nReboot for this to take effect!\nIf it does not work, please follow these instructions:\n\nedit CloudConfigurationDetails.plist and CloudConfigurationSetAsideDetails.plist. \nSet AllowPairing to true, \nConfigurationURL to https://example.com, \nisMDMUnremovable to 0, \nIsMandatory to false, and \nIsSupervised to false. \nThen, reboot and go into Setup. At the Remote Management step, press skip and you will be freed!'
